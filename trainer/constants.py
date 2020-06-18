c703744363b435f00523b8ea42a4fb97399febc5
Naveen Ram <naveenram00@gmail.com>
2020-06-18 19:16:51 +0000

    add license

--- trainer/constants.py
+++ trainer/constants.py
@@ -1,3 +1,17 @@
+# Copyright 2020 Google LLC
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Shared Constants."""
 import os
 
 INPUT_LENGTH = 512 #1033 - longest training input for redial
@@ -11,8 +25,8 @@ RD_SPLIT_FNAMES = {
     "train": "rd-train-formatted.jsonl",
     "validation": "rd-test-formatted.jsonl"
 }
-rd_counts_path = os.path.join(DATA_DIR, "rd-counts.json")
-rd_tsv_path = {
+RD_COUNTS_PATH = os.path.join(DATA_DIR, "rd-counts.json")
+RD_TSV_PATH = {
     "train": os.path.join(DATA_DIR, "rd-train.tsv"),
     "validation": os.path.join(DATA_DIR, "rd-validation.tsv")
 }
