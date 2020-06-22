a5ffc6f61ff6bf783b6ed06c67d49d0857c20aec
Naveen Ram <naveenram00@gmail.com>
2020-06-22 20:41:02 +0000

    reformat results

--- trainer/metrics.py
+++ trainer/metrics.py
@@ -21,6 +21,7 @@ import nltk
 import sacrebleu
 from trainer import constants
 import tensorflow_datasets as tfds
+import collections
 import json
 
 def _prediction_file_to_ckpt(path):
@@ -44,34 +45,37 @@ def load_predictions(task_name, model_dir):
   # Get most recent prediction file by sorting by their step.
   latest_prediction_file = sorted(
       prediction_files, key=_prediction_file_to_ckpt)[-1]
-
-  checkpoint_step =_prediction_file_to_ckpt(latest_prediction_file)
-
-  # Collect (inputs, targets, prediction) from the dataset and predictions file
-  results = []
+  
+  # results will store the inputs, targets, predictions and checkpoint_step in a dict
+  results = collections.defaultdict(list)
+  results["checkpoint_step"] = _prediction_file_to_ckpt(latest_prediction_file)
+  
+  # Collect inputs, targets, and predictions from the dataset and predictions file
   with tf.io.gfile.GFile(latest_prediction_file) as preds:
     for ex, pred in zip(tfds.as_numpy(ds), preds):
-      results.append((tf.compat.as_text(ex["inputs_plaintext"]),
-                      tf.compat.as_text(ex["targets_plaintext"]),
-                      pred.strip()))
-  return (results, checkpoint_step)
+      results["inputs"].append(tf.compat.as_text(ex["inputs_plaintext"]))
+      results["targets"].append(tf.compat.as_text(ex["targets_plaintext"]))
+      results["predictions"].append(pred.strip())
+  return results
 
 def save_metrics(task_name, model_dir):
-  """Prints and saves metrics for the most recent checkpoint."""
-  results, checkpoint_step = load_predictions(task_name, model_dir)
-  predictions = [line[2] for line in results]
-  targets = [line[1] for line in results]
-
+  """Prints and saves metrics for the most recent checkpoint. Current metrics are nltk bleu score and sacrebleu bleu score.
+  Data is lowercased before evaluation. Movie titles are not removed. """
+  results = load_predictions(task_name, model_dir)
+  predictions = results["predictions"]
+  targets = results["targets"]
+  print("PREDICTION: ", predictions[0])
+  print("TARGET: ", targets[0])
   hyp = list(map(lambda x: x.split(), predictions))
   ref = list(map(lambda x: [x.split()], targets))
 
   nltk_bs = nltk.translate.bleu_score.corpus_bleu(list_of_references=ref, hypotheses=hyp)
   sb_bs = str(sacrebleu.corpus_bleu(predictions, [targets]))
 
-  print("NLTK BLEU SCORE: {:f}, SACREBLEU BLEU SCORE: {:s}, CHECKPOINT: {:d}".format(nltk_bs, sb_bs, checkpoint_step))
+  print("NLTK BLEU SCORE: {:f}, SACREBLEU BLEU SCORE: {:s}, CHECKPOINT: {:d}".format(nltk_bs, sb_bs, int(results["checkpoint_step"])))
   # Writes to $MODEL_DIR$/validation_eval/metrics$CHECKPOINT_NUMBER.json
   metrics_path = os.path.join(
           model_dir,
-          "validation_eval/metrics" + str(checkpoint_step) + ".json")
+          "validation_eval/metrics" + str(results["checkpoint_step"]) + ".json")
   json.dump({"nltk_bleu_score" : nltk_bs, "sacrebleu_blue_score" : sb_bs, "recall@1" : 0}, tf.io.gfile.GFile(metrics_path, "w"))
 
